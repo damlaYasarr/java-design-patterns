933a3c4b800e1d25dba3228f6bddbbe60dc3ad39
damlaYasarr <damlayasar40@gmail.com>
2024-01-23 14:23:09 +0300

    deleted file

--- facade-converter/src/main/java/com/iluwatar/facades/AudioFormatConverter.java
+++
@@ -1,9 +0,0 @@
-package com.iluwatar.facades;
-
-public class AudioFormatConverter {
-    public String convert(String audio, String outputFormat) {
-        System.out.println("Audio format conversion: " + audio + " to " + outputFormat);
-        // Ses format dönüşüm işlemleri
-        return "Converted " + audio + " to " + outputFormat;
-    }
-}
--- facade-converter/src/main/java/com/iluwatar/facades/VideoFormatConverter.java
+++
@@ -1,9 +0,0 @@
-package com.iluwatar.facades;
-
-public class VideoFormatConverter {
-    public String convert(String video, String outputFormat) {
-        System.out.println("Video format conversion: " + video + " to " + outputFormat);
-        // Video format dönüşüm işlemleri
-        return "Converted " + video + " to " + outputFormat;
-    }
-}
